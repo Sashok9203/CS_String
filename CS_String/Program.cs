4389c3010d0b9cb86bf75e9c99f917051e8cf172
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-25 17:09:16 +0200

    Update

--- CS_String/Program.cs
+++ CS_String/Program.cs
@@ -7,9 +7,8 @@
             //Task 1
             Console.Clear();
             Console.WriteLine("  Task 1");
-            Console.Write(" Enter words : ");
-            string? str = Console.ReadLine();
-            string[] words = str.Split(' ');
+           
+            string[] words = getString(" Enter words : ").Split(' ');
             Array.Reverse(words);
             Console.Write(" Reversed words : ");
             foreach (var val in words)
@@ -20,16 +19,14 @@
             Console.Clear();
             Console.WriteLine("  Task 2");
             Console.Write(" Enter string with dots : ");
-            str = Console.ReadLine();
-            words = str.Split('.');
-            if(words.Length > 1)Console.Write($" String between dots : {words[1]}");
+            words = getString(" Enter string with dots : ").Split('.');
+            if (words.Length > 1)Console.Write($" String between dots : {words[1]}");
             Console.ReadKey();
 
             //Task 3
             Console.Clear();
             Console.WriteLine("  Task 3");
-            Console.Write(" Enter string : ");
-            str = Console.ReadLine();
+            string? str = getString(" Enter string : ");
             Console.WriteLine($" String \"{str}\" is{(new string(str.Reverse().ToArray()) == str ? "" : " not")} a palindrom...");
             Console.ReadKey();
 
@@ -38,8 +35,7 @@
             float lcPer;
             Console.Clear();
             Console.WriteLine("  Task 4");
-            Console.Write(" Enter string : ");
-            str = Console.ReadLine();
+            str = getString(" Enter string : ");
             foreach (char ch in str)
                 if (Char.IsLower(ch)) ++lowerCount;
             lcPer = (float)Math.Round(100f / str.Length * lowerCount,2);
@@ -47,15 +43,17 @@
             Console.ReadKey();
 
             //Task 5
+            int ind = 0;
             Console.Clear();
             Console.WriteLine("  Task 5");
-            Console.Write(" Enter text : ");
-            str = Console.ReadLine();
-            words = str.Split(new char[] { '.',' ',',',':','-'},StringSplitOptions.RemoveEmptyEntries);
+            words = getString(" Enter text : ").Split(new char[] { '.',' ',',',':','-'},StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
                 words[i].TrimStart();
-            Console.Write(" Index of word (1...): ");
-            int ind = int.Parse(Console.ReadLine());
+            Console.Write($" Index of word (1..{words.Length}): ");
+            do
+            {
+              ind = int.Parse(Console.ReadLine() ?? "0");
+            } while (ind < 1 || ind > words.Length);
             Console.WriteLine($" The first letter in the word \"{words[ind - 1]}\"  is \"{words[ind - 1][0]}\"");
             Console.ReadKey();
 
@@ -63,15 +61,14 @@
             Console.Clear();
             Console.WriteLine("  Task 6");
             Console.Write(" Words: ");
-            foreach(var val in words)
-                Console.Write($"{val} ");
-            Console.WriteLine();
             for (int i = 0; i < words.Length; i++)
             {
+                Console.Write($"{words[i]} ");
                 int len = words[i].Length;
                 if (len > 5)
-                   words[i] = words[i][..(len - 3)]+ "$$$";
+                   words[i] = words[i][..(len - 3)] + "$$$";
             }
+            Console.WriteLine();
             Console.Write(" Result words: ");
             foreach (var val in words)
                 Console.Write($"{val} ");
@@ -85,20 +82,25 @@
             Console.ReadKey();
 
             //Task 8
-            string result = "";
+            str = "";
             Console.Clear();
             Console.WriteLine("  Task 8");
-            Console.Write(" Enter text : ");
-            str = Console.ReadLine();
-            words = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            words = getString(" Enter text : ").Split(' ', StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
-                 result += words[i].Trim() + (i!= words.Length-1 ? '*' : ' ');
-            Console.WriteLine($" Result text :\n {result}");
+                str += words[i].Trim() + (i != words.Length-1 ? '*' : ' ');
+            Console.WriteLine($" Result text :\n {str}");
             Console.ReadKey();
-
-
         }
 
-
+        static string getString(string message)
+        {
+            string? str = "";
+            Console.Write(message);
+            do
+            {
+                str = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(str));
+            return str;
+        }
     }
 }
