9e8d0f039ecefc1e824d75f4d253a4d7ec8ba912
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-25 12:10:22 +0200

    Task 2-3

--- CS_String/Program.cs
+++ CS_String/Program.cs
@@ -4,19 +4,39 @@
     {
         static void Main(string[] args)
         {
+            //Task 1
+            Console.Clear();
+            Console.WriteLine("  Task 1");
             Console.Write(" Enter words : ");
             string? str = Console.ReadLine();
-            string[] words = StringRevers(ref str);
+            string[] words = str.Split(' ');
+            Array.Reverse(words);
             Console.Write(" Reversed words : ");
             foreach (var val in words)
                 Console.Write($"{val} ");
+            Console.ReadKey();
+
+            //Task 2
+            Console.Clear();
+            Console.WriteLine("  Task 2");
+            Console.Write(" Enter string with dots : ");
+            str = Console.ReadLine();
+            words = str.Split('.');
+            if(words.Length > 1)Console.Write($" String between dots : {words[1]}");
+            Console.ReadKey();
+
+            //Task 3
+            Console.Clear();
+            Console.WriteLine("  Task 3");
+            Console.Write(" Enter string : ");
+            str = Console.ReadLine();
+            //char[] reverse = str.ToCharArray();
+            //Array.Reverse(reverse);
+            Console.WriteLine($" String \"{str}\" is{(new string(str.Reverse().ToArray()) == str ? "" : " not")} a palindrom...");
+            Console.ReadKey();
+
         }
 
-        static string[] StringRevers( ref string str)
-        {
-            string[] words = str.Split(' ');
-            Array.Reverse(words);
-            return words;   
-        }
+
     }
 }
