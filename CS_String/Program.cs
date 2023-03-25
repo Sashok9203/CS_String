17002fb2a050b86b0e0f3c0c15c889e6815c4c00
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-25 18:05:58 +0200

    Task 7

--- CS_String/Program.cs
+++ CS_String/Program.cs
@@ -10,7 +10,7 @@
                 Console.WriteLine($"  Task {i}");
                 switch (i)
                 {
-                    case 1: Task1();break;
+                    case 1: Task1(); break;
                     case 2: Task2(); break;
                     case 3: Task3(); break;
                     case 4: Task4(); break;
@@ -33,6 +33,7 @@
             } while (string.IsNullOrWhiteSpace(str));
             return str;
         }
+
         static void Task1()
         {
             string[] words = getString(" Enter words : ").Split(' ');
@@ -41,10 +42,12 @@
             foreach (var val in words)
                 Console.Write($"{val} ");
         }
+
         static void Task2()
         {
             string[] words = getString(" Enter string with dots : ").Split('.');
-            if (words.Length > 1) Console.Write($" String between dots : {words[1]}");
+            if (words.Length > 2 && !string.IsNullOrWhiteSpace(words[1])) Console.Write($" String between dots : {words[1]}");
+            else Console.Write(" The string does not contain a dots or there are no characters between the dots");
         }
 
         static void Task3()
@@ -52,6 +55,7 @@
             string? str = getString(" Enter string : ");
             Console.WriteLine($" String \"{str}\" is{(new string(str.Reverse().ToArray()) == str ? "" : " not")} a palindrom...");
         }
+
         static void Task4()
         {
             int lowerCount = 0;
@@ -64,6 +68,7 @@
             lcPer = (float)Math.Round(100f / str.Length * lowerCount, 2);
             Console.WriteLine($" In string \"{str}\" {lcPer}% a lowercase letters and {100 - lcPer}% uppercase letters...");
         }
+
         static void Task5()
         {
             int ind;
@@ -73,13 +78,16 @@
             Console.Write($" Index of word (1..{words.Length}): ");
             do
             {
-                ind = int.Parse(Console.ReadLine() ?? "0");
+                try { ind = int.Parse(Console.ReadLine() ?? "0"); }
+                catch { ind = 0; }
             } while (ind < 1 || ind > words.Length);
             Console.WriteLine($" The first letter in the word \"{words[ind - 1]}\"  is \"{words[ind - 1][0]}\"");
         }
+
         static void Task6()
         {
             string[] words = getString(" Enter text : ").Split(new char[] { '.', ' ', ',', ':', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.Clear();
             Console.Write(" Words: ");
             for (int i = 0; i < words.Length; i++)
             {
@@ -93,16 +101,29 @@
             foreach (var val in words)
                 Console.Write($"{val} ");
         }
+
         static void Task7()
         {
+            string? str = "";
+            string[] words = getString(" Enter text : ").Split(new char[] { '.', ' ', ',', ':', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var val in words)
+                for (int i = 0; i < val.Length - 1; i++)
+                    if (val.Length > 1 && Char.IsUpper(val[i]) && Char.IsUpper(val[i + 1]))
+                    {
+                        str += val + " ";
+                        break;
+                    }
+            if (!string.IsNullOrEmpty(str)) Console.WriteLine($" Abbreviations : {str}");
+            else Console.WriteLine($" No abbreviations in text...");
         }
+
         static void Task8()
         {
             string? str = "";
-            string []words = getString(" Enter text : ").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string []words = getString(" Enter text : ").Split(new char[] { ' ','\t' }, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
                 str += words[i].Trim() + (i != words.Length - 1 ? '*' : ' ');
-            Console.WriteLine($" Result text :\n {str}");
+            Console.Write($" Result text :\n {str}");
         }
     }
-    }
+}
