4e173f55539ac75bd86e653de304132fb8b6a25c
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-25 11:18:32 +0200

    Task 1

--- CS_String/Program.cs
+++ CS_String/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter words : ");
+            string? str = Console.ReadLine();
+            string[] words = StringRevers(ref str);
+            Console.Write(" Reversed words : ");
+            foreach (var val in words)
+                Console.Write($"{val} ");
+        }
+
+        static string[] StringRevers( ref string str)
+        {
+            string[] words = str.Split(' ');
+            Array.Reverse(words);
+            return words;   
         }
     }
 }
